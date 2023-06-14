2ea3f0ffe89324a2ab113ffb2ab339dd4d4a82ea
TatianaNabatova <135061136+TatianaNabatova@users.noreply.github.com>
2023-06-14 18:57:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -100,8 +100,8 @@ def main():
     EstimatedSalary = st.sidebar.slider("Зарплата", 0.00, 200000.00)
     
     #if st.button('Сделать прогноз'):
-        output = predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
-        st.success('Вероятность оттока составляет {}'.format(output))
+        #output = predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
+        #st.success('Вероятность оттока составляет {}'.format(output))
      
     
     
