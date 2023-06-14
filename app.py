8c40bc9a719850acccce8d0563187a52c5da1850
TatianaNabatova <135061136+TatianaNabatova@users.noreply.github.com>
2023-06-14 17:34:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -108,13 +108,13 @@ def main():
    if Balance < 1000 and EstimatedSalary < 500 and IsActiveMember == 0 and NumOfProducts == 1:
             st.success('Вероятность оттока составляет более 90%.')
             st.markdown(churn_html, unsafe_allow_html= True)
-            if Salary < 100:
+   if Salary < 100:
         EstimatedSalary = 100
-    elif Salary >= 100 and Salary < 6000:
+   elif Salary >= 100 and Salary < 6000:
         EstimatedSalary = Salary
-    else:
+   else:
         EstimatedSalary = 6000
-    if st.button('Сделать прогноз'):
+   if st.button('Сделать прогноз'):
         output = predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
         st.success('Вероятность оттока составляет {}'.format(output))
       
